ef07cb32a1767d700f29fdc6658f370d62ee44d4
Mats Iremark <mats.x.iremark@collectorbank.se>
2016-11-25 14:10:18 +0100

    Added WebjobsName and WebjobsType enrichers.

---
+++ test/Serilog.Enrichers.AzureWebApps.UnitTest/AzureWebJobsNameEnricherTest.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using NUnit.Framework;
+using Serilog.Events;
+
+namespace Serilog.Enrichers.AzureWebApps.UnitTest
+{
+    public class AzureWebJobsNameEnricherTest
+    {
+        [Test]
+        public void Name_is_taken_from_environment_variables()
+        {
+            Environment.SetEnvironmentVariable("WEBJOBS_NAME", "MyWebjob");
+
+            LogEvent logEvent = null;
+            var logger = new LoggerConfiguration()
+                .Enrich.With(new AzureWebJobsNameEnricher())
+                .WriteTo.Sink(new DelegatingSink(e => logEvent = e))
+                .CreateLogger();
+
+            logger.Information("Test log");
+
+            Assert.AreEqual("MyWebjob", ((ScalarValue)logEvent.Properties["AzureWebJobsName"]).Value);
+        }
+
+        [Test]
+        public void Name_is_no_webjob_when_environment_variable_is_not_set()
+        {
+            LogEvent logEvent = null;
+            var logger = new LoggerConfiguration()
+                .Enrich.With<AzureWebJobsNameEnricher>()
+                .WriteTo.Sink(new DelegatingSink(e => logEvent = e))
+                .CreateLogger();
+
+            logger.Information("Test log");
+
+            Assert.AreEqual("NO_WEBJOB", ((ScalarValue)logEvent.Properties["AzureWebJobsName"]).Value);
+
+        }
+        
+    }
+}
---
+++ test/Serilog.Enrichers.AzureWebApps.UnitTest/AzureWebJobsTypeEnricherTest.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using NUnit.Framework;
+using Serilog.Events;
+
+namespace Serilog.Enrichers.AzureWebApps.UnitTest
+{
+    public class AzureWebJobsTypeEnricherTest
+    {
+        [Test]
+        public void Type_is_taken_from_environment_variables()
+        {
+            Environment.SetEnvironmentVariable("WEBJOBS_TYPE", "Continious");
+
+            LogEvent logEvent = null;
+            var logger = new LoggerConfiguration()
+                .Enrich.With(new AzureWebJobsTypeEnricher())
+                .WriteTo.Sink(new DelegatingSink(e => logEvent = e))
+                .CreateLogger();
+
+            logger.Information("Test log");
+
+            Assert.AreEqual("Continious", ((ScalarValue)logEvent.Properties["AzureWebJobsType"]).Value);
+        }
+
+        [Test]
+        public void Type_is_no_webjob_when_environment_variable_is_not_set()
+        {
+            LogEvent logEvent = null;
+            var logger = new LoggerConfiguration()
+                .Enrich.With<AzureWebJobsTypeEnricher>()
+                .WriteTo.Sink(new DelegatingSink(e => logEvent = e))
+                .CreateLogger();
+
+            logger.Information("Test log");
+
+            Assert.AreEqual("NO_WEBJOB", ((ScalarValue)logEvent.Properties["AzureWebJobsType"]).Value);
+
+        }
+        
+    }
+}
